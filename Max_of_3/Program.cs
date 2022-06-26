aa894de2ff55d1ffd3610ff248a7f428f6f83228
Prateek Pai <paiprateek123@gmail.com>
2022-06-26 14:36:28 +0530

    Find the Max in the given 3 Strings

--- Max_of_3/Program.cs
+++ Max_of_3/Program.cs
@@ -6,6 +6,7 @@ namespace Max_of_3
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Find Max From the Given Integer");
             //Test Case-1 :  Max at 1st Position
             Console.WriteLine(Program.MaxIntegerNumber(65, 25, 55));
 
@@ -15,6 +16,9 @@ namespace Max_of_3
             //Test Case-3 : Max at 3rd Position
             Console.WriteLine(Program.MaxIntegerNumber(78, 43, 92));
 
+            Console.WriteLine("............................................");
+            Console.WriteLine("Find Max From the Given Floats");
+
             //Test Case-1 : Max at 1st Position
             Console.WriteLine(Program.MaxFloatNumber(8.32f, 5.84f, 7.63f));
 
@@ -23,6 +27,18 @@ namespace Max_of_3
 
             //Test Case-3 : Max at 3rd Position
             Console.WriteLine(Program.MaxFloatNumber(6.74f, 3.56f, 9.81f));
+
+            Console.WriteLine("............................................");
+            Console.WriteLine("Find Max From the Given Strings");
+
+            //Test Case-1 :Max at 1st Position
+            Console.WriteLine(Program.MaxStringNumber("69", "28", "45"));
+
+            //Test Case-2 :Max at 2nd Position
+            Console.WriteLine(Program.MaxStringNumber("62", "75", "49"));
+
+            //Test Case-3 :Max at 3rd Position
+            Console.WriteLine(Program.MaxStringNumber("21", "18", "35"));
         }
         // Creating method to find the max from the given integer
         public static int MaxIntegerNumber(int FirstValues, int SecondValue, int ThirdValue)
@@ -50,6 +66,30 @@ namespace Max_of_3
 
         // Creating method to find the max from the given Float
         public static float MaxFloatNumber(float FirstValues, float SecondValue, float ThirdValue)
+        {
+            if (FirstValues.CompareTo(SecondValue) > 0 && FirstValues.CompareTo(ThirdValue) > 0 ||
+               FirstValues.CompareTo(SecondValue) >= 0 && FirstValues.CompareTo(ThirdValue) > 0 ||
+               FirstValues.CompareTo(SecondValue) > 0 && FirstValues.CompareTo(ThirdValue) >= 0)
+            {
+                return FirstValues;
+            }
+            if (SecondValue.CompareTo(FirstValues) > 0 && SecondValue.CompareTo(ThirdValue) > 0 ||
+                SecondValue.CompareTo(FirstValues) >= 0 && SecondValue.CompareTo(ThirdValue) > 0 ||
+                SecondValue.CompareTo(FirstValues) > 0 && SecondValue.CompareTo(ThirdValue) >= 0)
+            {
+                return SecondValue;
+            }
+            if (ThirdValue.CompareTo(FirstValues) > 0 && ThirdValue.CompareTo(SecondValue) > 0 ||
+                ThirdValue.CompareTo(FirstValues) >= 0 && ThirdValue.CompareTo(SecondValue) > 0 ||
+                ThirdValue.CompareTo(FirstValues) > 0 && ThirdValue.CompareTo(SecondValue) >= 0)
+            {
+                return ThirdValue;
+            }
+            return FirstValues;
+        }
+
+        // Creating method to find the max from the given Strings
+        public static string MaxStringNumber(string FirstValues, string SecondValue, string ThirdValue)
         {
             if (FirstValues.CompareTo(SecondValue) > 0 && FirstValues.CompareTo(ThirdValue) > 0 ||
                FirstValues.CompareTo(SecondValue) >= 0 && FirstValues.CompareTo(ThirdValue) > 0 ||
